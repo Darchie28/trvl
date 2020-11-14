7f162051ed4567868f71c0269f0d49cb9a001f4c
Denzel Archie <archie_denzel@yahoo.com>
2020-11-14 04:12:30 -0600

    added hero section, included video playback in background

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
 import './NavBar.css';
 import {Button} from './Button';
@@ -18,13 +18,18 @@ function NavBar() {
         }
     }
 
+    useEffect(() => {
+        showButton();
+    }, [])
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                   <Link to="/" className="navbar-logo">
+                   <Link to="/" className="navbar-logo"
+                   onClick={closeMobileMenu}>
                        TRVL <i className='fab fa-typo3'></i>
                    </Link>
                    <div className='menu-icon' onClick={handleClick}>
