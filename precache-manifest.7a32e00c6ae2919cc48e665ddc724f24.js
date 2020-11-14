a4c9cf3f07f21849268300628f2dadb2ef16ff51
Denzel Archie <archie_denzel@yahoo.com>
2020-11-14 12:51:34 -0600

    Updates

---
+++ precache-manifest.7a32e00c6ae2919cc48e665ddc724f24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdd2a5470b346730628172bbb8a9a26",
+    "url": "./https://darchie28.github.io/trvl/index.html"
+  },
+  {
+    "revision": "b6897170dd4ea379d609",
+    "url": "./https://darchie28.github.io/trvl/static/css/main.29c7f2f3.chunk.css"
+  },
+  {
+    "revision": "c6a832d25dade59b2188",
+    "url": "./https://darchie28.github.io/trvl/static/js/2.777c46aa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./https://darchie28.github.io/trvl/static/js/2.777c46aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6897170dd4ea379d609",
+    "url": "./https://darchie28.github.io/trvl/static/js/main.cbdcae24.chunk.js"
+  },
+  {
+    "revision": "4a83b8fa1d1a3bac37a9",
+    "url": "./https://darchie28.github.io/trvl/static/js/runtime-main.f3f9de73.js"
+  }
+]);
