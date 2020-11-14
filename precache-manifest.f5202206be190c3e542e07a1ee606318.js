e7cc8f3bf843ab264254b2fdf385995b08957a75
Denzel Archie <archie_denzel@yahoo.com>
2020-11-14 10:46:47 -0600

    Updates

---
+++ precache-manifest.f5202206be190c3e542e07a1ee606318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afb98fb7c628c29b10a59cbf521525b",
+    "url": "/trvl/index.html"
+  },
+  {
+    "revision": "32efaf804501705092b5",
+    "url": "/trvl/static/css/main.066107e3.chunk.css"
+  },
+  {
+    "revision": "c6a832d25dade59b2188",
+    "url": "/trvl/static/js/2.777c46aa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trvl/static/js/2.777c46aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32efaf804501705092b5",
+    "url": "/trvl/static/js/main.ae601659.chunk.js"
+  },
+  {
+    "revision": "529b196655f16097fa8a",
+    "url": "/trvl/static/js/runtime-main.41976432.js"
+  }
+]);
